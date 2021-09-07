d7b9e44fe3ddc0e4021bd3406a60c5103ad3980a
Joanna <joanna.lillo@efrei.net>
2021-09-07 16:55:03 +0200

    Blue -> Record and write it
    Purple -> Speech ( Hello/Goodbye)

---
+++ VoiceInteraction/Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+
+[RequireComponent(typeof(Collider))]
+public class ClickHandler : MonoBehaviour
+{
+    public UnityEvent upEvent;
+    public UnityEvent downEvent;
+    void OnMouseDown()
+    {
+        Debug.Log(" ******  Down !  ****** ");
+        downEvent?.Invoke();
+
+    }
+    void OnMouseUp()
+    {
+        Debug.Log("  ******  Up ! ******  ");
+        upEvent?.Invoke();
+    }
+}
---
+++ VoiceInteraction/Assets/Scripts/SpeechController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TextSpeech;
+using UnityEngine.Android;
+
+public class SpeechController : MonoBehaviour
+{
+    const string LANG_CODE = "en-US";
+
+    [SerializeField]
+    Text uiText;
+
+    private void Start()
+    {
+        Setup(LANG_CODE);
+
+        SpeechToText.instance.onPartialResultsCallback = OnPartialSpeechResult;
+        SpeechToText.instance.onResultCallback = OnFinalSpeechResults;
+        TextToSpeech.instance.onStartCallBack = OnSpeakStart;
+        TextToSpeech.instance.onDoneCallback = OnSpeakStop;
+        CheckPermission();
+    }
+
+    void CheckPermission()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+    }
+    /// <summary>
+    /// --------------------Text To Speech----------------
+    /// </summary>
+
+
+    public void StartSpeaking(string message)
+    {
+        TextToSpeech.instance.StartSpeak(message);
+    }
+
+    public void StopSpeaking()
+    {
+        TextToSpeech.instance.StopSpeak();
+    }
+
+    void OnSpeakStart()
+    {
+        Debug.Log("I'm hearing you ! ");
+    }
+
+    void OnSpeakStop()
+    {
+        Debug.Log("WHAT ?! I CAN'T HEAR YOU ! IS IT Finished ... Oh Lord !");
+    }
+
+
+
+    /// <summary>
+    /// ---------------Speech To Text----------------
+    /// </summary>
+    public void StartListening()
+    {
+        SpeechToText.instance.StartRecording();
+    }
+
+    public void StopListening()
+    {
+        SpeechToText.instance.StopRecording();
+    }
+
+   void OnFinalSpeechResults(string result)
+    {
+        uiText.text = result;
+    }
+
+    void OnPartialSpeechResult(string result)
+    {
+        uiText.text = result;
+    }
+    void Setup(string code)
+    {
+        TextToSpeech.instance.Setting(code, 1, 1);
+        SpeechToText.instance.Setting(code);
+    }
+}
